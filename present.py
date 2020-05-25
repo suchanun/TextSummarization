a22aa5634630af5b39708d4ab29d2540728cf6af
Suchanun Piriyasatit <s.piriyasatit@gmail.com>
2020-05-25 21:16:04 +0700

    load nltk data in main

--- present.py
+++ present.py
@@ -6,19 +6,21 @@ from utils_smart_stat import *
 
 st.sidebar.title("What to do")
 app_mode = st.sidebar.selectbox("Choose the app mode",
-        ["Simple tf-idf", "Smart stat with tf-idf", "Show the source code"])
+        ["Simple tf-idf", "Smart stat with tf-idf"])
 colors_dict = {0:'#D0F15F',1:'#90F9E3',2:'#E9B2ED'}
 
 def main():
     st.title('Rank sentences by {}'.format(app_mode))
-    st.markdown('<p style="font-size:17px"><span style="background-color: {}">My summary</span>'.format(colors_dict[0]),
-                unsafe_allow_html=True)
-    st.markdown(
-        '<p style="font-size:17px"><span style="background-color: {}">Their summary</span>'.format(colors_dict[1]),
-        unsafe_allow_html=True)
-    st.markdown('<p style="font-size:17px"><span style="background-color: {}">Both</span>'.format(colors_dict[2]),
-                unsafe_allow_html=True)
     if app_mode == 'Simple tf-idf':
+
+        st.markdown(
+            '<p style="font-size:17px"><span style="background-color: {}">My summary</span>'.format(colors_dict[0]),
+            unsafe_allow_html=True)
+        st.markdown(
+            '<p style="font-size:17px"><span style="background-color: {}">Their summary</span>'.format(colors_dict[1]),
+            unsafe_allow_html=True)
+        st.markdown('<p style="font-size:17px"><span style="background-color: {}">Both</span>'.format(colors_dict[2]),
+                    unsafe_allow_html=True)
         run_app_simple()
     elif app_mode == 'Smart stat with tf-idf':
         run_app_smart()
@@ -28,44 +30,56 @@ def main():
 def run_app_smart():
 
     #presentation.set_dataset(dataset)
-    dataset = st.radio('Dataset', ('Newsroom', 'CNN'))
-    n_sentences = st.number_input('number of sentences in summary', value=5, min_value=1)
-    #presentation.set_n_sentences(n_sentences)
-    n_articles = 1000
+    dataset = st.sidebar.radio('Dataset', ( 'CNN', 'Custom Input')) #'Newsroom',
+    apply_location = st.sidebar.checkbox("Apply location stat")
+    one_minus_k = st.sidebar.slider(
+        'keywords: important ---> diverse',
+        0.0, 1.0, value=0.8#(25.0, 75.0)
+    )
+    k = 1-one_minus_k
+    m = st.sidebar.slider(
+        'm: Select a range of values',
+        0.0, 1.0, value=0.15  # (25.0, 75.0)
+    )
 
-    article_number = st.number_input(label='{} article article number'.format(dataset), value=0,
-                                     format='%d', max_value=n_articles - 1, min_value=0)
+    n_sentences = st.sidebar.number_input('number of sentences in summary', value=5, min_value=1)
+    if dataset != 'Custom Input':
+        n_articles = 1000
 
-    st.markdown('or')
-    random = st.button('random an article')
+        article_number = st.sidebar.number_input(label='{} article article number'.format(dataset), value=0,
+                                                 format='%d', max_value=n_articles - 1, min_value=0)
+
     data = load_data()
     ranker = data['smart_ranker']
 
-    # class Displayer:
-    #     @staticmethod
-    #     def show_cnn(info, n_sentences):
-    #
-    # # info['text'] == cnn_ref_text[doc_i]
-    # # info['my_model_result'] == cnn_tfidf_ret[doc_i]
-    # # info['presumm_result'] == cnn_presumm_ret[doc_i]
-    # # info['cnn_ref_summary'] == cnn_ref_summaries[doc_i]
 
-    if random:
+    if dataset == 'CNN':
+        st.sidebar.markdown('or')
+        random = st.sidebar.button('random an article')
+        if random:
+            doc_i = np.random.randint(1000)
+        else:
+            doc_i = article_number
 
-        doc_i = np.random.randint(1000)
         st.header('{} {}'.format(dataset, doc_i))
-        # print(type(doc_i))
+
         info = dict()
         info['text'] = data['cnn_ref_text'][doc_i]
-        info['my_model_result'] = ranker.rank_sentences(info['text'])
 
-        print(info['my_model_result'])
+        info['my_model_result'] = ranker.rank_sentences(info['text'],k=k,m=m)
+
         info['presumm_result'] = data['cnn_presumm_ret'][doc_i]
         info['cnn_ref_summary'] = data['cnn_ref_summaries'][doc_i]
         Displayer.show_cnn(info,n_sentences)
-    else:
-        print(type(article_number))
-        #presentation.show(article_number,n_sentences)
+    elif dataset == 'Custom Input':
+
+        custom_text = st.text_area('text to be summarized',height=400)
+        if  custom_text != '':
+            info = dict()
+            info['text'] = custom_text
+            info['my_model_result'] = ranker.rank_sentences(custom_text,k=k,m=m)
+            Displayer.show_custom(info, n_sentences)
+
 
 def run_app_simple():
 
@@ -82,30 +96,33 @@ def run_app_simple():
     # st.markdown('<p style="font-size:17px"><span style="background-color: {}">Their summary</span>'.format(colors_dict[1]),unsafe_allow_html=True)
     # st.markdown('<p style="font-size:17px"><span style="background-color: {}">Both</span>'.format(colors_dict[2]),unsafe_allow_html=True)
 
-    dataset = st.radio('Dataset',('Newsroom','CNN'))
+    dataset = st.sidebar.radio('Dataset',('Newsroom','CNN'))
     presentation.set_dataset(dataset)
-    n_sentences = st.number_input('number of sentences in summary',value=5,min_value=1)
+    n_sentences = st.sidebar.number_input('number of sentences in summary',value=5,min_value=1)
     presentation.set_n_sentences(n_sentences)
     n_articles = 1000
 
-    article_number = st.number_input(label='{} article article number'.format(dataset),value=0,
+    article_number = st.sidebar.number_input(label='{} article article number'.format(dataset),value=0,
         format='%d',max_value=n_articles-1,min_value=0)
 
-    st.markdown('or')
-    random = st.button('random an article')
+    st.sidebar.markdown('or')
+    random = st.sidebar.button('random an article')
 
     if random:
         doc_i = np.random.randint(1000)
 
-        print(type(doc_i))
+        # print(type(doc_i))
         presentation.show(doc_i)
 
     else:
 
-        print(type(article_number))
+        # print(type(article_number))
         presentation.show(article_number)
 @st.cache(allow_output_mutation=True)
 def load_data():
+    nltk.download('punkt')
+    nltk.download('wordnet')
+    nltk.download('stopwords')
     # load_data
     # newsroom
     data = dict()
